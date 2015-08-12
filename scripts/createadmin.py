48e6b14f44425ccf3345454bc889e3cf89ae4e5c
Avinash Soni <avinash.soni@gmail.com>
2015-08-12 23:56:05 +0530

    dfffs

--- scripts/createadmin.py
+++ scripts/createadmin.py
@@ -1,6 +1,6 @@
 #
 from django.contrib.auth.models import User
-if User.objects.count() == 0:
+if User.objects.count()==0:
     admin = User.objects.create(username='admin')
     admin.set_password('admin')
     admin.is_superuser = True
