f1cd9b0446913bb4afb1f3b4517707b626a9b429
Avinash Soni <avinash.soni@gmail.com>
2015-08-12 22:56:11 +0530

    mm

--- appink/settings.py
+++ appink/settings.py
@@ -95,12 +95,17 @@ WSGI_APPLICATION = 'appink.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite'),
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
-}
 
 
 # Internationalization
@@ -135,7 +140,7 @@ STATICFILES_DIRS = (
     #'C:/Users/yogesh soni/Desktop/appink/uploads',
 )
 MEDIA_DIRS = (
-	
+
 )
 
 
